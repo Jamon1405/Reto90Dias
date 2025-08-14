9ff5b28bd3ea53d2da53d137e2d2ccdae2ab0a33
Juan Antonio <antonioarvizuag@gmail.com>
2025-08-13 20:13:28 -0600

    Add basic mobile responsiveness

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,11 @@ export const metadata = {
   description: 'Producción audiovisual inspirada en colorfilms.mx',
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
